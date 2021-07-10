52e758b9993778921d2168ef9c9c8fe31abb3437
H3lltronik <esau.egs1@gmail.com>
2021-07-09 23:04:45 -0500

    updated base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: 'https://h3lltronik.where-in-the-world.netlify.app/'
+  base: 'https://h3lltronik--where-in-the-world.netlify.app/'
 })
